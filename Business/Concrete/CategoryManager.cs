93239231910aba3f6c0a5415dc9d533b28e75681
KADİR <kadircengiz909@gmail.com>
2025-09-26 18:24:56 +0300

    26.09.2025 gün sonu

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using DataAcces.Concrete.Repositories;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo= new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+            {
+                //hata mesajı
+                Console.WriteLine("Kategori adı boş geçilemez, 3 karakterden az ve 51 karakterden fazla olamaz.");
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+
+    }
+}
---
+++ DataAcces/Concrete/Repositories/CateogryRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAcces.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Concrete.Repositories
+{
+    public class CateogryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        DbSet<Category> _object;
+        public void Delete(Category category)
+        {
+            _object.Remove(category);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category category)
+        {
+            _object.Add(category);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            c.SaveChanges();
+        }
+    }
+}
